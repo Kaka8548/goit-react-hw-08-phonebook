9d06cf60e86fbbe3b01a3fb5755a9a2e621dce10
Kaka8548 <katyakatya8548@gmail.com>
2023-02-01 09:55:02 +0400

    private and restricted routes was added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,6 +7,8 @@ import Layout from './layout/Layout';
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { refreshUser } from 'redux/auth/operations';
+import { RestrictedRoute } from './RestrictedRoute';
+import { PrivateRoute } from './PrivateRoute';
 
 export function App() {
   const dispatch = useDispatch();
@@ -19,9 +21,24 @@ export function App() {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />}></Route>
-        <Route path="login" element={<Login />} />
-        <Route path="register" element={<Register />} />
-        <Route path="contacts" element={<Contacts />} />
+        <Route
+          path="login"
+          element={
+            <RestrictedRoute redirectTo="/contacts" component={<Login />} />
+          }
+        />
+        <Route
+          path="register"
+          element={
+            <RestrictedRoute redirectTo="/contacts" component={<Register />} />
+          }
+        />
+        <Route
+          path="contacts"
+          element={
+            <PrivateRoute redirectTo="/login" component={<Contacts />} />
+          }
+        />
         <Route path="*" element={<Navigate to={'/'} />} />
       </Route>
     </Routes>
